827134a3c4193ab537d195fae542f2870b7cc8ab
AceHunterr <aggarwalmehul26@gmail.com>
2023-08-06 06:24:23 +0530

    accordion error zzzzzzzzzzz

--- src/components/QuestionForm/QuestionForm.jsx
+++ src/components/QuestionForm/QuestionForm.jsx
@@ -18,6 +18,10 @@ import { FCRightUp } from "react-icons/fc";
 import CloseIcon from "@material-ui/icons/Close";
 import Radio from "@material-ui/core/Radio";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Typography from "@mui/material/Typography"; // Import Typography from Material-UI
+
+import { MenuItem } from "@material-ui/core"; // Import MenuItem from Material-UI
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
 
 import CropOriginalIcon from "@material-ui/icons/CropOriginal";
 import ShortTextIcon from "@material-ui/icons/ShortText";
@@ -25,6 +29,147 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
 
 const QuestionForm = () => {
+  const [questions, setQuestions] = useState([
+    {
+      questionText: "Which is the capital city of karnataka?",
+      questionType: "radio",
+      options: [
+        { optionText: "Bengaluru" },
+        { optionText: "Belgavi" },
+        { optionText: "Hubli" },
+        { optionText: "Mandya" },
+      ],
+      open: true,
+      required: false,
+    },
+  ]);
+
+  function questionsUI() {
+    return questions.map((ques, i) => {
+      <Accordion expanded={ques.open} className={ques.open ? "add border" : ""}>
+        <AccordionSummary
+          aria-controls="panella-content"
+          id="panella-header"
+          elevation={1}
+          style={{ width: "100%" }}
+        >
+          {!questions[i].open ? (
+            <div className="saved_questions">
+              <Typography
+                style={{
+                  fontSize: "15px",
+                  fontWeight: "400",
+                  letterSpacing: ".1px",
+                  lineHeight: "24px",
+                  paddingBottom: "8px",
+                }}
+              >
+                {i + 1}. {questions[i].questionText}
+              </Typography>
+
+              {ques.options.map((op, j) => (
+                <div key={j}>
+                  <div style={{ display: "flex" }}>
+                    <FormControlLabel
+                      style={{ marginLeft: "5px", marginBottom: "5px" }}
+                      disabled
+                      control={
+                        <input
+                          type={ques.questionType}
+                          color="primary"
+                          style={{ marginRight: "3px" }}
+                          required={ques.type}
+                        />
+                      }
+                      label={
+                        <Typography
+                          style={{
+                            fontFamily: "Roboto, Arial, sans-serif",
+                            fontSize: "13px",
+                            fontweight: "400",
+                            letterSpacing: ".2px",
+                            lineHeight: "20px",
+                            color: "#202124",
+                          }}
+                        >
+                          {ques.options[j].optionText}
+                        </Typography>
+                      }
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            ""
+          )}
+        </AccordionSummary>
+        <div className="question_boxes">
+          <AccordionDetails className="add_question">
+            <div className="add_question_top">
+              <input
+                type="text"
+                className="question"
+                placeholder="Question"
+                value={ques.questionText}
+              />
+              <CropOriginalIcon style={{ color: "#5f6368" }} />
+              <Select
+                className="select"
+                style={{ color: "#5f6368", fontSize: "13px" }}
+              >
+                <MenuItem id="text" value="Text">
+                  {" "}
+                  <SubjectIcon style={{ marginRight: "10px" }} /> Paragraph
+                </MenuItem>
+                <MenuItem id="checkbox" value="Checkbox">
+                  <CheckBoxIcon
+                    style={{ marginRight: "10px", color: "#70757a" }}
+                    checked
+                  />{" "}
+                  Checkboxes
+                </MenuItem>
+                <MenuItem id="radio" value="Radio">
+                  {" "}
+                  <Radio
+                    style={{ marginRight: "10px", color: "#70757a" }}
+                    checked
+                  />{" "}
+                  Multiple Choice
+                </MenuItem>
+              </Select>
+            </div>
+
+            {ques.options.map((op, j) => (
+              <div className="add_question_body" key={j}>
+                {ques.questionType != "text" ? (
+                  <input
+                    type={ques.questionType}
+                    style={{ marginRight: "10px" }}
+                  />
+                ) : (
+                  <ShortTextIcon style={{ marginRight: "10px" }} />
+                )}
+                <div>
+                  <input
+                    type="text"
+                    className="text_input"
+                    placeholder="option"
+                    value={ques.options[j].optionText}
+                  ></input>
+                </div>
+                <CropOriginalIcon style={{ color: "#5f6368" }} />
+                <IconButton aria-label="delete">
+                  <CloseIcon />
+                </IconButton>
+              </div>
+            ))}
+          </AccordionDetails>
+        </div>
+      </Accordion>;
+    });
+  }
+
   return (
     <>
       <div>
@@ -46,6 +191,8 @@ const QuestionForm = () => {
                 ></input>
               </div>
             </div>
+
+            {questionsUI()}
           </div>
         </div>
       </div>
