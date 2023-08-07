f4c1788b9c3c68b8392287a04c8cdc6a41f92fc2
AceHunterr <aggarwalmehul26@gmail.com>
2023-08-07 17:52:48 +0530

    Added the fillblank question skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import FormHeader from "./components/FormHeader/FormHeader";
 import QuestionForm from "./components/QuestionForm/QuestionForm";
 import CategorizedForm from "./components/CategoryQuestion/CategoryQuestion";
+import DraggableComponent from "./components/DraggableComponent/DraggableComponent";
+import ClozeQuestion from "./components/ClozeQuestion/ClozeQuestion";
+import "./index.css";
 function App() {
   return (
     <div className="App">
-      {/* <FormHeader />
-      <QuestionForm /> */}
-      <CategorizedForm />
+      <FormHeader />
+      {/* <QuestionForm /> */}
+      <ClozeQuestion />
+      {/* <CategorizedForm /> */}
+      {/* <DraggableComponent /> */}
     </div>
   );
 }
---
+++ src/components/DraggableComponent/model.jsx
@@ -0,0 +1,7 @@
+const Todo = {
+  id: 0,
+  todo: "",
+  isDone: false,
+};
+
+export default Todo;
