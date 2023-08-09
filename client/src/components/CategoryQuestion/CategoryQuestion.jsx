9306b9a1b03815ac77b42188697f11d8badd0a39
AceHunterr <aggarwalmehul26@gmail.com>
2023-08-09 14:35:49 +0530

    Added the remaining builder functionality for category question

--- client/src/components/CategoryQuestion/CategoryQuestion.jsx
+++ client/src/components/CategoryQuestion/CategoryQuestion.jsx
@@ -16,6 +16,8 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { BsTrash } from "react-icons/bs";
 import NorthEastIcon from "@mui/icons-material/NorthEast";
 import { MenuItem } from "@material-ui/core";
+import FilterNoneIcon from "@material-ui/icons/FilterNone";
+import CloseIcon from "@material-ui/icons/Close";
 
 const CategoryQuestion = () => {
   const [categoryQuestion, setCategoryQuestion] = useState([
@@ -24,14 +26,14 @@ const CategoryQuestion = () => {
       categories: [
         {
           category: "Cars",
-          objects: [{ objectText: "Asdfsdf" }, { objectText: "asdasdB" }],
+          objects: [{ objectText: "Car A" }, { objectText: "Car B" }],
         },
         {
           category: "Bikes",
           objects: [
-            { objectText: "C" },
-            { objectText: "D" },
-            { objectText: "E" },
+            { objectText: "Bike A" },
+            { objectText: "Bike B" },
+            { objectText: "Bike C" },
           ],
         },
       ],
@@ -51,8 +53,8 @@ const CategoryQuestion = () => {
     setCategoryQuestion(updatedCategoryQuestion);
   }
   function changeCategoryText(text, i, j) {
-    console.log("Hello");
-    console.log(categoryQuestion[i].categories[j].category);
+    // console.log("Hello");
+    // console.log(categoryQuestion[i].categories[j].category);
     var newCategoryQuestion = [...categoryQuestion];
     var newCategories = [...newCategoryQuestion[i].categories];
     newCategories[j].category = text;
@@ -75,6 +77,37 @@ const CategoryQuestion = () => {
       ...newCategories[j],
       objects: newObjects,
     };
+
+    newCategoryQuestion[i] = {
+      ...newCategoryQuestion[i],
+      categories: newCategories,
+    };
+
+    setCategoryQuestion(newCategoryQuestion);
+  }
+
+  function addCategory(i) {
+    var newCategoryQuestion = [...categoryQuestion];
+    var newCategories = [...newCategoryQuestion[i].categories];
+
+    newCategories.push({
+      category: "New Cat",
+      objects: [{ objectText: "New Obj 1" }, { objectText: "New Obj 2" }],
+    });
+
+    newCategoryQuestion[i] = {
+      ...newCategoryQuestion[i],
+      categories: newCategories,
+    };
+
+    setCategoryQuestion(newCategoryQuestion);
+  }
+
+  function removeCategory(i, j) {
+    var newCategoryQuestion = [...categoryQuestion];
+    var newCategories = [...newCategoryQuestion[i].categories];
+
+    newCategories.splice(j, 1);
 
     newCategoryQuestion[i] = {
       ...newCategoryQuestion[i],
@@ -238,24 +271,33 @@ const CategoryQuestion = () => {
                 </Typography>
 
                 {ques.categories.map((category, j) => (
-                  <div key={j}>
-                    <span>{j + 1}. </span>
-                    <input
-                      id="options-text"
-                      type="text"
-                      className="text_input category_categories"
-                      placeholder="Category"
-                      value={category.category}
-                      onChange={(e) => {
-                        changeCategoryText(e.target.value, i, j);
-                      }}
-                      style={{
-                        fontSize: "16px",
-                        fontWeight: "600",
-                        marginTop: "10px",
-                        textTransform: "uppercase",
-                      }}
-                    />
+                  <div>
+                    <div key={j}>
+                      <span>{j + 1}. </span>
+                      <input
+                        id="options-text"
+                        type="text"
+                        className="text_input category_categories"
+                        placeholder="Category"
+                        value={category.category}
+                        onChange={(e) => {
+                          changeCategoryText(e.target.value, i, j);
+                        }}
+                        style={{
+                          fontSize: "16px",
+                          fontWeight: "600",
+                          marginTop: "10px",
+                          textTransform: "uppercase",
+                        }}
+                      />
+                      <IconButton aria-label="delete">
+                        <CloseIcon
+                          onClick={() => {
+                            removeCategory(i, j);
+                          }}
+                        />
+                      </IconButton>
+                    </div>
                     {category.objects.map((object, k) => (
                       <div
                         className="add_question_body"
@@ -310,8 +352,103 @@ const CategoryQuestion = () => {
                     </div>
                   </div>
                 ))}
-                {/* Other sections of your accordion */}
+                <div style={{ paddingLeft: "2px" }}>
+                  {/* <input
+                    id="options-text"
+                    type="text"
+                    className="text_input"
+                    style={{ fontSize: "13px", width: "60px" }}
+                    placeholder="Add other"
+                  ></input> */}
+                  <Button
+                    size="large"
+                    onClick={() => {
+                      addCategory(i);
+                    }}
+                    style={{
+                      textTransform: "none",
+                      color: "#4285f4",
+                      fontsize: "13px",
+                      fontweight: "600",
+                      marginTop: "10px",
+                    }}
+                  >
+                    Add Category
+                  </Button>
+                </div>
+
+                <div className="add_footer">
+                  <div className="add_question_bottom_left">
+                    <Button
+                      size="small"
+                      style={{
+                        textTransform: "none",
+                        color: "#4285f4",
+                        fontsize: "13px",
+                        fontweight: "600",
+                      }}
+                    >
+                      <NorthEastIcon
+                        style={{
+                          border: "2px solid #4285f4",
+                          padding: "2px",
+                          marginRight: "8px",
+                        }}
+                      />
+                      Answer key
+                    </Button>
+                  </div>
+                  <div className="add_question_bottom">
+                    <IconButton
+                      aria-label="Copy"
+                      onClick={() => {
+                        copyQuestion(i);
+                      }}
+                    >
+                      <FilterNoneIcon />
+                    </IconButton>
+                    <IconButton
+                      aria-label="delete"
+                      onClick={() => {
+                        deleteQuestion(i);
+                      }}
+                    >
+                      <BsTrash />
+                    </IconButton>
+                    <span style={{ color: "#5f6368", fontsize: "13px" }}>
+                      Required
+                    </span>
+                    <Switch
+                      name="checkedA"
+                      color="primary"
+                      onClick={() => {
+                        requiredQuestion(i);
+                      }}
+                    />
+                    <IconButton>
+                      <MoreVertIcon />
+                    </IconButton>
+                  </div>
+                </div>
+                {/* <Button
+              variant="contained"
+              color="primary"
+              onClick={savecomprehensionQuestions}
+              style={{ width: "50%" }}
+            >
+              Save Form Data
+            </Button> */}
               </AccordionDetails>
+              <div className="question_edit">
+                <AddCircleOutlineIcon
+                  className="edit"
+                  style={{ fontSize: 38 }}
+                  onClick={addMoreQuestionField}
+                />
+                <OndemandVideoIcon className="edit" style={{ fontSize: 38 }} />
+                <CropOriginalIcon className="edit" style={{ fontSize: 38 }} />
+                <TextFieldsIcon className="edit" style={{ fontSize: 38 }} />
+              </div>
             </div>
           </Accordion>
         </div>
